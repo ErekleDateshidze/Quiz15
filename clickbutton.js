c907a81dfdec25c851abfb49295b3208ffc5d68f
Erekle99 <erekledateshidze99@gmail.com>
2022-06-10 19:57:26 +0400

    1

--- clickbutton.js
+++ clickbutton.js
@@ -1,13 +1,33 @@
 
-var button = document.getElementById("clickbutton");
-count = 0;
-button.onclick = function () {
-    count += 1;
-    button.innerHTML = "clickbutton :" + count;
-    if (count >= 10) {
-        button.onclick = function () {
-            count -= 1;
-            button.innerHTML = "clickbutton :" + count;
-        }
+// var button = document.getElementById("clickbutton");
+// count = 0;
+// button.onclick = function () {
+//     count += 1;
+//     button.innerHTML = "click button :" + count;
+//     if (count >= 10) {
+//         button.onclick = function () {
+//             count -= 1;
+//             button.innerHTML = "click button :" + count;
+//         }
+//     }
+//     button.onclick = function () {
+//         count += 1;
+//         button.innerHTML = "click button :" + count;
+//     }
+// }
+
+
+
+var buttonclick = document.getElementById("clickbutton")
+var minusplus = 1;
+var count = 0;
+
+
+buttonclick.onclick = function () {
+    count += minusplus;
+    buttonclick.innerHTML = "click button :" + count;
+    if (count == 10 || count == 0) {
+        minusplus *= -1;
+        buttonclick.innerHTML = "click button :" + count;
     }
 }
